c5f690fc916d6fb185664d5210b4cfcded878156
Lee Ho Sung <43498096+BengaleeHS@users.noreply.github.com>
2021-05-23 01:31:06 +0900

    Update auth.ts

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -64,7 +64,7 @@ export default (app: Router) => {
 				const authServiceInstance = new AuthService();
 				const recordServiceInstance = new RecordService();
 				const { user } = await authServiceInstance.GetUser(req.body);
-				console.log(user.game_id);
+				console.log(user);
 				const {rating} = await recordServiceInstance.GetUserRating(user.game_id,user.user_id);
 				return res.status(200).json({ ...user, ...rating  });
 			} catch (e) {
