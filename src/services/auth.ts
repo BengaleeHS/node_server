650e9d29a1136f275f1d189cf2b2078b8338c04b
luceinaltis <luceinaltis2020@naver.com>
2021-05-22 18:16:32 +0900

    auth update

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -14,112 +14,70 @@ import { IUser, IUserInputDTO } from '../interfaces/IUser';
  */
 export default class AuthService {
 	/**
-	 *	크리모의 새로운 유저를 추가한다.
+	 *	새로운 유저를 추가한다.
 	 * @param userInputDTO 유저가 입력한 유저 정보
-	 * @returns { user, token } 생성된 유저 정보, 토큰
+	 * @returns { user } 생성된 유저 정보, 토큰
 	 */
-	public async SignUp(
-		userInputDTO: IUserInputDTO,
-	): Promise<{ user: IUser; token: string }> {
+	public async SignUp(userInputDTO: IUserInputDTO): Promise<{ user: IUser }> {
 		try {
 			/* 모든 정보가 입력되었는지 확인한다 */
-			if (
-				typeof userInputDTO.email === 'undefined' ||
-				typeof userInputDTO.name === 'undefined' ||
-				typeof userInputDTO.password === 'undefined'
-			) {
+			if (typeof userInputDTO.user_name === 'undefined') {
 				const err = new Error('There is information not entered');
 				err['status'] = 400;
 				throw err;
 			}
 
-			const salt = randomBytes(32);
+			let userRecord = User.create({ ...userInputDTO });
 
-			const hashedPassword = await argon2.hash(userInputDTO.password, { salt });
-			let userRecord = User.create({
-				...userInputDTO,
-				role: 2,
-				salt: salt.toString('hex'),
-				password: hashedPassword,
+			const getSameUsername = await User.find({
+				user_name: userInputDTO.user_name,
 			});
 
-			const getSameEmail = await User.find({
-				email: userInputDTO.email,
-			});
-
-			/* 데이터베이스에 email이 존재하는지 확인한다 */
-			if (getSameEmail.length) {
-				throw new Error('This email is already being used');
+			/* 데이터베이스에 같은 username이 존재하는지 확인한다 */
+			if (getSameUsername.length) {
+				throw new Error('This user name is already being used');
 			}
 
 			await userRecord.save();
-			const token = this.generateToken(userRecord);
-
-			/**
-			 * @TODO 완벽한 3계층 레이어가 아니다.
-			 * controller - service - data access
-			 * 추후 service와 data access를 나눌 필요성이 있다.
-			 * 유지관리를 위한 것이니 굳이 할 필요는 없습니다.
-			 */
 			const user = JSON.parse(JSON.stringify(userRecord));
-			Reflect.deleteProperty(user, 'password');
-			Reflect.deleteProperty(user, 'salt');
-			Reflect.deleteProperty(user, 'id');
-			return { user, token };
+
+			return { user };
 		} catch (e) {
 			throw e;
 		}
 	}
 
 	/**
-	 * 계정을 입력하고 로그인을 진행한다.
+	 * 유저 이름을 입력하고 로그인을 진행한다.
 	 * @param email 유저의 이메일
 	 * @param password 유저의 패스워드
 	 */
-	public async SignIn(
-		email: string,
-		password: string,
-	): Promise<{ user: IUser; token: string }> {
-		const userRecord = await User.findOne({ email });
+	public async SignIn(userName: string): Promise<{ user: IUser }> {
+		const userRecord = await User.findOne({ user_name: userName });
 		if (!userRecord) {
 			throw new Error('User not registered');
 		}
-		/**
-		 * We use verify from argon2 to prevent 'timing based' attacks
-		 */
-		const validPassword = await argon2.verify(userRecord.password, password);
-		if (validPassword) {
-			const token = this.generateToken(userRecord);
 
-			const user = JSON.parse(JSON.stringify(userRecord));
-			Reflect.deleteProperty(user, 'password');
-			Reflect.deleteProperty(user, 'salt');
-			Reflect.deleteProperty(user, 'id');
-			/**
-			 * Easy as pie, you don't need passport.js anymore :)
-			 */
-			return { user, token };
-		} else {
-			throw new Error('Invalid Password');
-		}
+		userRecord.is_login = true;
+
+		await userRecord.save();
+
+		const user = JSON.parse(JSON.stringify(userRecord));
+		return { user };
 	}
 
-	/**
-	 * 현재 서버 시간으로부터 4시간 동안 유효한 JWT토큰을 생성한다.
-	 * @param user 토큰을 생성 하고자하는 유저 정보
-	 */
-	private generateToken(user: IUser) {
-		const today = new Date();
-		const exp = new Date(today);
-		exp.setHours(today.getHours() + 4);
+	public async SignOut(userName: string): Promise<{ user: IUser }> {
+		const userRecord = await User.findOne({ user_name: userName });
+		if (!userRecord) {
+			throw new Error('User not registered');
+		}
 
-		return jwt.sign(
-			{
-				id: user.id, // We are gonna use this in the middleware 'isAuth'
-				email: user.email,
-				exp: exp.getTime() / 1000,
-			},
-			config.jwtSecret,
-		);
+		userRecord.is_login = false;
+
+		await userRecord.save();
+
+		const user = JSON.parse(JSON.stringify(userRecord));
+
+		return { user };
 	}
 }
