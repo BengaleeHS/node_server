31c050bd6be8486b013c1d51d131a4390cd7d088
Lee Ho Sung <43498096+BengaleeHS@users.noreply.github.com>
2021-05-23 02:20:06 +0900

    Update auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -109,7 +109,7 @@ export default class AuthService {
 	 * @param userName 유저 이름
 	 */
 	public async GetUser(userInputDTO:IUserInputDTO): Promise<{ user: IUser}> {
-		console.log(userInputDTO);
+		console.log({...userInputDTO});
 		const userRecord = await User.findOne({ ...userInputDTO });
 		if (!userRecord) {
 			throw new Error('User not registered');
