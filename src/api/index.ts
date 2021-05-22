9eac2550a3e3d0dfd725c5f23e9aaeeae9d32d00
Lee Ho Sung <43498096+BengaleeHS@users.noreply.github.com>
2021-05-22 19:15:51 +0900

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import auth from './routes/auth';
+import record from './routes/record';
 
 export default () => {
 	const app = Router();
 	auth(app);
-
+	record(app)
 	return app;
 };
