acaa8fdd73a6b63537bf949df6ef0e8a5ca3fb81
Lee Ho Sung <43498096+BengaleeHS@users.noreply.github.com>
2021-05-22 18:34:18 +0900

    Update index.ts

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,8 +2,8 @@ import expressLoader from './express';
 import postgreLoader from './postgreSQL';
 
 export default async ({ expressApp }) => {
-	// await postgreLoader();
-	// console.log('✅ Database loaded');
+	await postgreLoader();
+	console.log('✅ Database loaded');
 
 	await expressLoader({ app: expressApp });
 	console.log('✅ Express loaded');
